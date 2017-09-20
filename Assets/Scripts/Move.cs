246ef7f3532563b0a0bc38a3d7b00d5bc464c829
GuyWallah <nathan.prokaska@ocsbstudent.ca>
2017-09-20 11:17:16 -0400

    Level 3

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -27,6 +34,16 @@ public class Move : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "You Win!";
         }
     }
 }
